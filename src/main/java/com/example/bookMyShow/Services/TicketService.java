9efd888480832a3063d5ddc749bed5294f8ccc6b
svenkatesh009 <117891450+svenkatesh009@users.noreply.github.com>
2023-07-06 06:52:09 +0530

    Update TicketService.java

--- src/main/java/com/example/bookMyShow/Services/TicketService.java
+++ src/main/java/com/example/bookMyShow/Services/TicketService.java
@@ -100,7 +100,7 @@ public class TicketService {
                 "Enjoy the Show !!!";
 
         simpleMessageMail.setSubject("Ticket Confirmation Mail");
-        simpleMessageMail.setFrom("springacciojob@gmail.com");
+        simpleMessageMail.setFrom("projecttestingtesting333@gmail.com");
         simpleMessageMail.setText(body);
         simpleMessageMail.setTo(user.getEmail());
 
